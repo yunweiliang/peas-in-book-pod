7eb827ba44538e1a8c6279357ee6e5f803b1bac6
Yunwei Liang <yunweilia@gmail.com>
2020-09-12 10:49:16 -0700

    restore not addtional components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PodComponent } from './pod.component';
-import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PodComponent,
-    UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
